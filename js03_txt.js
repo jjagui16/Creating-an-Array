10ebc634b78f348e1587e3f88474fa7ae144ce72
jjagui16 <joseaguilar16@my.lonestar.edu>
2026-02-26 19:37:43 -0600

    Creating an Array

---
+++ js03_txt.js
@@ -0,0 +1,12 @@
+/*    JavaScript 7th Edition
+    Chapter 3
+    Chapter case
+
+    Tipton Turbines
+    Program to display games results in a web table
+    Author: Jose Aguilar
+    Date:   02/26/2026
+
+    Filename: js03.js
+ */
+
